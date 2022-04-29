0f190ed17d5060bc30eb03d7ac0d10bf06702096
Simon Schäfer <schaefer@embedded.rwth-aachen.de>
2022-04-29 10:48:29 +0200

    Fixing linking error on Ubuntu 22.04.

--- cmake/FindPugiXml.cmake
+++ cmake/FindPugiXml.cmake
@@ -1,8 +1,12 @@
-find_library(PUGIXML_LIBRARIES NAMES pugixml)
-find_path(
-        PUGIXML_INCLUDE_DIRS
-        NAMES pugixml.hpp
-        PATH_SUFFIXES pugixml)
+find_library(PugiXml_LIBRARIES NAMES pugixml)
+find_path(PugiXml_INCLUDE_DIRS NAMES pugixml.hpp PATH_SUFFIXES pugixml)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(pugixml DEFAULT_MSG PUGIXML_LIBRARIES PUGIXML_INCLUDE_DIRS)
+find_package_handle_standard_args(PugiXml DEFAULT_MSG PugiXml_LIBRARIES PugiXml_INCLUDE_DIRS)
+
+if (PugiXml_FOUND)
+    add_library(PugiXml INTERFACE IMPORTED)
+    target_link_libraries(PugiXml INTERFACE ${PugiXml_LIBRARIES})
+    target_include_directories(PugiXml INTERFACE ${PugiXml_INCLUDE_DIRS})
+endif ()
+
